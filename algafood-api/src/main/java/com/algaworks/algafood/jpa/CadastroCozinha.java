3c4828c957ed2d0f2438bb3e308dbc1eb3cd7f9e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-26 17:28:31 -0400

    feat: Add example using ApplicationContext and EntityManager, for testing purposes.

---
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	// Anotação @PersistenceContext para injetar entity manager.
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public List<Cozinha> listar() {
+		// Utilizando JPQL para obter entidades de cozinha.
+		return manager.createQuery("from Cozinha", Cozinha.class)
+				.getResultList();
+	}
+	
+}
