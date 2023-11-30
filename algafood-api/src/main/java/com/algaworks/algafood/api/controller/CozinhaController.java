cb97ed594391513427712072eccc9df8170f37f0
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-11-29 21:58:53 -0400

    feat: Add REST controller for Cozinha resource.

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaReposity;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	private final CozinhaReposity cozinhaReposity;
+	
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaReposity.listar();
+	}
+	
+}
