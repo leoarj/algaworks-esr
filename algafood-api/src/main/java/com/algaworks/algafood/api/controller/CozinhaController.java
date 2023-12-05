f257fa37743dc97ace5009d6f9010f2d13506f95
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-12-04 21:45:10 -0400

    feat: Add example of use ResponseEntity.

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,7 +2,10 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,8 +45,23 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaReposity.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		//return cozinhaReposity.buscar(cozinhaId);
+		Cozinha cozinha = cozinhaReposity.buscar(cozinhaId);
+		
+		// ResponseEntity pode ser utilizado para se ter um melhor controle da resposta HTTP.
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		//return ResponseEntity.ok(cozinha);
+		
+		// Classe HttpHeaders pode ser utilizada para definição de cabeçalhos na resposta.
+		// HttpHeaders possui cabeçalhos padrão que podem ser utilizados. 
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
 	}
 	
 }
