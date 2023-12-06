994cda40425186da989a96926037d440455d13d0
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-12-05 22:47:22 -0400

    feat: Add endpoint for create resource of Restaurante.

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CadastroRestauranteService {
+
+	private final RestauranteRepository restauranteRepository;
+	private final CozinhaRepository cozinhaRepository;
+	
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+		
+		restaurante.setCozinha(cozinha); // Atualiza restaurante com cozinha vinda do respositório (managed).
+		
+		return restauranteRepository.salvar(restaurante);
+	}
+	
+}
