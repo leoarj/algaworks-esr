b641dc4d152466384a7b02eabb319a9125da197b
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-12-09 16:07:45 -0400

    feat: Add implementation for update resource.
    Add implementation for update rersource using reflection API.

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -112,18 +114,51 @@ public class RestauranteController {
 			return ResponseEntity.notFound().build();
 		}
 		
-		merge(campos, restauranteAtual);
+		//merge(campos, restauranteAtual);
+		restauranteAtual = merge2(campos, restauranteAtual);
 		
 		return atualizar(restauranteId, restauranteAtual);
 	}
 
 	/**
 	 * Método para tratar a atualização parcial do recurso com base nas propriedades recebidas.
+	 * 
+	 * Reflection pode ser utilizada para manipular as propriedades alteradas.
 	 */
-	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
-		camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			System.out.println(nomePropriedade + " = " + valorPropriedade);
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		// Mapeador para transformar propriedades de um corpo de requição para um objeto de uma classe especificada.
+		ObjectMapper objectMapper = new ObjectMapper();
+		// Converte o corpo JSON preenchendo em um objeto as propriedades correspondentes de mesmo nome definidos na classe.
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			// Obtém um representação referente a um campo da classe
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			// Para permitir acessar o campo privado
+			field.setAccessible(true);
+			
+			// Obtém valor contido no campo e altera no objeto convertido inicialmente
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+			System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
+			
+			// Altera no objeto de destino o valor obtido do objeto convertido (origem)
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
 		});
+	}
+	
+	// Sugestão de implementação sem usar reflection
+	private Restaurante merge2(Map<String, Object> dadosOrigem, Restaurante restauranteAtual) {
+		// Mapeador para transformar propriedades de um corpo de requição para um objeto de uma classe especificada.
+		ObjectMapper objectMapper = new ObjectMapper();
+		// Converte o corpo JSON preenchendo em um objeto as propriedades correspondentes de mesmo nome definidos na classe.
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		// Extrai quais propriedades atualizadas, para que ao repassar dados do restaurante atual
+		// as propriedades atualizadas do restaurante convertido do corpo JSON não sejam alteradas de volta ao valor atual.
+		String[] propriedadesIgnorar = dadosOrigem.keySet().toArray(String[]::new); //size -> new String[size]
+		BeanUtils.copyProperties(restauranteAtual, restauranteOrigem, propriedadesIgnorar);
+		return restauranteOrigem;
 		
 	}
 
