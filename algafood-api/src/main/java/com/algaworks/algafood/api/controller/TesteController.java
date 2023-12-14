55b670535ea8597079e65d74bca9cf9742a6ceec
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-12-13 20:36:17 -0400

    feat: Add example of search by parameter with JPQL
    Add example of search by parameter in repository using JPQL and RequestParam.

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+	private final CozinhaRepository cozinhaRepository;
+
+	// Utilizando @RequestParam para injetar parâmetro da requisição no argumento
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.consultarPorNome(nome);
+	}
+}
