c3f1d2cb5fa55e10b51280b739c390361a258b50
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-12-18 20:26:35 -0400

    feat: Add a simple example of JPA Criteria API

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -29,39 +31,56 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
-//		var jpql = "from Restaurante where nome like :nome " +
-//				"and taxaFrete between :taxaInicial and :taxaFinal";
+////		var jpql = "from Restaurante where nome like :nome " +
+////				"and taxaFrete between :taxaInicial and :taxaFinal";
+//		
+//		var jpql = new StringBuilder();
+//		jpql.append("from Restaurante where 0 = 0 ");
+//		
+//		var parametros = new HashMap<String, Object>();
+//		
+//		if (StringUtils.hasLength(nome)) {
+//			jpql.append("and nome like :nome ");
+//			parametros.put("nome", "%" + nome + "%");
+//		}
+//		
+//		if (taxaInicial != null) {
+//			jpql.append("and taxaFrete >= :taxaInicial ");
+//			parametros.put("taxaInicial", taxaInicial);
+//		}
+//		
+//		if (taxaFinal != null) {
+//			jpql.append("and taxaFrete <= :taxaFinal");
+//			parametros.put("taxaFinal", taxaFinal);
+//		}
+//		
+////		return manager.createQuery(jpql.toString(), Restaurante.class)
+////				.setParameter("nome", "%" + nome + "%")
+////				.setParameter("taxaInicial", taxaInicial)
+////				.setParameter("taxaFinal", taxaFinal)
+////				.getResultList();
+//		
+//		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+//	
+//		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+//		
+//		return query.getResultList();
 		
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
+		/**
+		 * Exemplo com Criteria API do JPA.
+		 * Com criteria, é possível estabelecer consultas programaticamente.
+		 * Geralmente é mais utilizada com consultas mais complexas,
+		 * devido a quantidade de código necessária para fazer funcionar.
+		 * 
+		 * Para consultas mais simples JPQL e métodos de conveniência do Spring
+		 * ainda são uma boa opção.
+		 */
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		var parametros = new HashMap<String, Object>();
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		criteria.from(Restaurante.class);
 		
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
-		
-		if (taxaInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaInicial);
-		}
-		
-		if (taxaFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal");
-			parametros.put("taxaFinal", taxaFinal);
-		}
-		
-//		return manager.createQuery(jpql.toString(), Restaurante.class)
-//				.setParameter("nome", "%" + nome + "%")
-//				.setParameter("taxaInicial", taxaInicial)
-//				.setParameter("taxaFinal", taxaFinal)
-//				.getResultList();
-		
-		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-	
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-		
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
 
