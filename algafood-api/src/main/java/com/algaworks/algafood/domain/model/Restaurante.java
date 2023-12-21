dfecbb59b357cb475548f7395371fb9ceada3e6e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-12-20 21:31:50 -0400

    feat: Add example of ManyToMany mapping

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -9,9 +11,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -35,4 +40,16 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false) // Não necessário, apenas para referência.
 	private Cozinha cozinha;
+	
+	/**
+	 * @JoinTable para definir a tabela de associação.
+	 * @joinColumns para definir as colunas que apontam para cada chave primária de cada tabela.
+	 * joinColumns para definir nome da chave estrangeira referente a primeira entidade.
+	 * inverseJoinColumns para definir nome da chave estrangeira referente a segunda entidade.
+	 */
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 }
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,11 +1,5 @@
-/*
-insert into cozinha (nome) values ('Japonesa');
-insert into cozinha (nome) values ('Portuguesa');
-
-INSERT INTO `algafood`.`restaurante` (`nome`, `taxa_frete`, cozinha_id) VALUES ('Do Japão', '5', 1);
-INSERT INTO `algafood`.`restaurante` (`nome`, `taxa_frete`, cozinha_id) VALUES ('Restaurante Tokio', '7', 1);
-INSERT INTO `algafood`.`restaurante` (`nome`, `taxa_frete`, cozinha_id) VALUES ('Restaurante Português', '5.50', 2);
-*/
+insert into cozinha (id, nome) values (1, 'Tailandesa');
+insert into cozinha (id, nome) values (2, 'Indiana');
 
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gourmet', 10, 1);
 insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
@@ -28,3 +22,5 @@ insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
