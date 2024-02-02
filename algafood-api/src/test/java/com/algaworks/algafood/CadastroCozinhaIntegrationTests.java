8caf2d94ab29b673f902558945a23d757c69eaf8
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-02-01 22:48:43 -0400

    feat: Add integration test for save Cozinha

---
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,68 @@
+package com.algaworks.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+
+/**
+ * Classe de testes para testar cadastro de cozinha.
+ * 
+ * Os testes seguem um padrão de nomenclatura, que deve dizer o sentido do teste.
+ * 
+ * Os testes são basicamente definidos em:
+ * - Cenário = Atores (objetos) relacionados no teste.
+ * - Ação = Interação entre os objetos que deverá produzir os resultados a serem validados.
+ * - Validação = Verificação dos resultados obtidos a partir da ação,
+ * onde os resultados já são esperados em um determinado estado.
+ */
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinhaService;
+	
+	/**
+	 * Não deve falhar para o caso de cozinha ser informada corretamente para cadastro.
+	 * Nesse caso, garantir que a operação ocorra sem falhas para dados informados corretamente.
+	 */
+	@Test
+	void testarCadastroCozinhaComSucesso() {
+		// cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		// ação
+		novaCozinha = cadastroCozinhaService.salvar(novaCozinha);
+		
+		// validação
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
+	/**
+	 * Deve falhar para o caso de cozinha ser informada incorretamente para cadastro.
+	 * Nesse caso, garantir que um erro seja capturado.
+	 */
+	@Test
+	void testarCadastroCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+		
+		// Necessário devido ao JUnit 5
+		ConstraintViolationException erroEsperado =
+				Assertions.assertThrows(ConstraintViolationException.class, () -> {
+					cadastroCozinhaService.salvar(novaCozinha);
+				});
+		
+		assertThat(erroEsperado).isNotNull();
+	}
+
+}
