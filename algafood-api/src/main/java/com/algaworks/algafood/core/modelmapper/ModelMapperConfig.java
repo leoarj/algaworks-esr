cc867858ea8119a20c11381df8a1c037a5bf96ba
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-06-25 20:58:41 -0400

    feat: Add address to the Restaurante resource
    Add address to the Restaurante resource representation model.

--- algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,7 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 /**
@@ -20,6 +22,15 @@ public class ModelMapperConfig {
 		// Exemplo quando necessitar de algum mapeamento específico
 //		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
 //			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete)
+		
+		var enderecoToEndercoModelTypeMap = modelMapper
+				.createTypeMap(Endereco.class, EnderecoModel.class);
+		
+		enderecoToEndercoModelTypeMap.<String>addMapping(
+				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(), 
+				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+		
+		
 		
 		return modelMapper;
 	}
