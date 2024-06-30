6107b47a26769ed535795cfb6d2a1efffea07afd
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-06-30 19:52:19 -0400

    feat: Add endpoints to associate sub-resources
    Add endpoints to associate Produto sub-resource to Restaurante resource.

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
@@ -0,0 +1,64 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.api.assembler.ProdutoModelAssembler;
+import com.algaworks.algafood.api.disassembler.ProdutoInputDisassembler;
+import com.algaworks.algafood.api.model.ProdutoModel;
+import com.algaworks.algafood.api.model.input.ProdutoInput;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos")
+public class RestauranteProdutoController {
+	
+	private final ProdutoModelAssembler produtoModelAssembler;
+	private final ProdutoInputDisassembler produtoInputDisassembler;
+	
+	private final CadastroProdutoService cadastroProdutoService;
+	
+	@GetMapping
+	public List<ProdutoModel> listar(@PathVariable Long restauranteId) {
+		return produtoModelAssembler.toCollectionModel(
+				cadastroProdutoService.listarOuFalharPorCodigoRestaurante(restauranteId));
+	}
+	
+	@GetMapping("/{produtoId}")
+	public ProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+		return produtoModelAssembler.toModel(cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId));
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ProdutoModel adicionar(@RequestBody @Valid ProdutoInput produtoInput, @PathVariable Long restauranteId) {
+		Produto produto = produtoInputDisassembler.toDomainObject(produtoInput);
+		
+		return produtoModelAssembler.toModel(cadastroProdutoService.salvarNovo(produto, restauranteId));
+	}
+	
+	@PutMapping("/{produtoId}")
+	public ProdutoModel atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+			@RequestBody @Valid ProdutoInput produtoInput) {
+		Produto produtoAtual = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
+		
+		produtoInputDisassembler.copyToDomainObject(produtoInput, produtoAtual);
+		
+		return produtoModelAssembler.toModel(cadastroProdutoService.salvar(produtoAtual));
+	} 
+}
