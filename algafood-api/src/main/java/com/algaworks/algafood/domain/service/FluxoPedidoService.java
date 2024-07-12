486f5a5a4d5c019d3813ecdd29a38426b8df206e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-11 22:02:09 -0400

    refactor: Add business logic to domain model
    Move business logic concerning confirmation, cancellation and delivery
    to the domain model.

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -1,13 +1,9 @@
 package com.algaworks.algafood.domain.service;
 
-import java.time.OffsetDateTime;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.model.StatusPedido;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,40 +16,19 @@ public class FluxoPedidoService {
 	@Transactional
 	public void confirmar(Long pedidoId) {
 		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-		validarStatusPedido(pedido, StatusPedido.CRIADO, StatusPedido.CONFIRMADO);
-
-		pedido.setStatus(StatusPedido.CONFIRMADO);
-		pedido.setDataConfirmacao(OffsetDateTime.now());
+		pedido.confirmar();
 	}
 
 	@Transactional
 	public void cancelar(Long pedidoId) {
 		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-		validarStatusPedido(pedido, StatusPedido.CRIADO, StatusPedido.CANCELADO);
-
-		pedido.setStatus(StatusPedido.CANCELADO);
-		pedido.setDataCancelamento(OffsetDateTime.now());
+		pedido.cancelar();
 	}
 
 	@Transactional
 	public void entregar(Long pedidoId) {
 		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-		validarStatusPedido(pedido, StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE);
-
-		pedido.setStatus(StatusPedido.ENTREGUE);
-		pedido.setDataEntrega(OffsetDateTime.now());
-	}
-
-	private void validarStatusPedido(Pedido pedido, StatusPedido statusEsperado, StatusPedido statusDestino) {
-		if (!pedido.getStatus().equals(statusEsperado)) {
-			throw new NegocioException(
-					String.format("Status do pedido %d não pode ser alterado de %s para %s",
-					pedido.getId(), pedido.getStatus().getDescricao(),
-					statusDestino.getDescricao()));
-		}
+		pedido.entregar();
 	}
 
 }
