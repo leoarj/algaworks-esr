45f9c347134b4a5600968b815a3ef3684c2be63f
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-11 21:46:32 -0400

    Add endpoints to cancel or deliver orders

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -16,20 +16,44 @@ import lombok.RequiredArgsConstructor;
 public class FluxoPedidoService {
 
 	private final EmissaoPedidoService emissaoPedidoService;
-	
+
 	@Transactional
-	public void confirmar(Long pedidodId) {
-		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidodId);
-		
-		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-			throw new NegocioException(
-					String.format("Status do pedido %d não pode ser alterado de %s para %s",
-							pedido.getId(), pedido.getStatus().getDescricao(),
-							StatusPedido.CONFIRMADO.getDescricao()));
-		}
-		
+	public void confirmar(Long pedidoId) {
+		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
+
+		validarStatusPedido(pedido, StatusPedido.CRIADO, StatusPedido.CONFIRMADO);
+
 		pedido.setStatus(StatusPedido.CONFIRMADO);
 		pedido.setDataConfirmacao(OffsetDateTime.now());
+	}
+
+	@Transactional
+	public void cancelar(Long pedidoId) {
+		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
+
+		validarStatusPedido(pedido, StatusPedido.CRIADO, StatusPedido.CANCELADO);
+
+		pedido.setStatus(StatusPedido.CANCELADO);
+		pedido.setDataCancelamento(OffsetDateTime.now());
+	}
+
+	@Transactional
+	public void entregar(Long pedidoId) {
+		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
+
+		validarStatusPedido(pedido, StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE);
+
+		pedido.setStatus(StatusPedido.ENTREGUE);
+		pedido.setDataEntrega(OffsetDateTime.now());
+	}
+
+	private void validarStatusPedido(Pedido pedido, StatusPedido statusEsperado, StatusPedido statusDestino) {
+		if (!pedido.getStatus().equals(statusEsperado)) {
+			throw new NegocioException(
+					String.format("Status do pedido %d não pode ser alterado de %s para %s",
+					pedido.getId(), pedido.getStatus().getDescricao(),
+					statusDestino.getDescricao()));
+		}
 	}
 
 }
