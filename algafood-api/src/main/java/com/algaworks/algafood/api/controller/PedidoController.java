a5aa3c01d3ecbd5410b16f8e26e6ccc5dfab0a5e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-15 20:33:39 -0400

    refactor: Removes JsonView usage

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +25,6 @@ import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -47,28 +42,9 @@ public class PedidoController {
 	private final PedidoInputDisassembler pedidoInputDisassembler;
 
 	@GetMapping
-	public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
-		List<Pedido> pedidos = pedidoRepository.findAll();
-		List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidos);
-		
-		MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
-		
-		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-		filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
-		
-		if (StringUtils.isNotBlank(campos)) {
-			filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
-		}
-		
-		pedidosWrapper.setFilters(filterProvider);
-		
-		return pedidosWrapper;
+	public List<PedidoResumoModel> listar() {
+		return pedidoResumoModelAssembler.toCollectionModel(pedidoRepository.findAll());
 	}
-	
-//	@GetMapping
-//	public List<PedidoResumoModel> listar() {
-//		return pedidoResumoModelAssembler.toCollectionModel(pedidoRepository.findAll());
-//	}
 	
 	@GetMapping("/{codigoPedido}")
 	public PedidoModel buscar(@PathVariable String codigoPedido) {
