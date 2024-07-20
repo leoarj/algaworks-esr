dcb6c0789d3dde8818245802e4a49b9b8327e953
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-20 16:02:02 -0400

    feat: Add pagination and sorting in Pedidos resource
    Add pagination and sorting in Pedidos resource and fix the PedidosSpec
    for execute count query correctly.

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/spec/PedidoSpecs.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/spec/PedidoSpecs.java
@@ -18,9 +18,23 @@ public class PedidoSpecs {
 	public static Specification<Pedido> usandoFiltro(PedidoFilter filtro) {
 		return (root, query, builder) -> {
 			
-			// Para evitar N+1
-			root.fetch("restaurante").fetch("cozinha");
-			root.fetch("cliente");
+			/*
+			 * Condição para verificar o tipo de resutado da query.
+			 * Só vai realizar o fetch com as outras entidades para Pedido se for uma query de Pedido.
+			 * 
+			 * O objetivo é evitar a exception abaixo:
+			 * 	"org.hibernate.QueryException:
+			 *  query specified join fetching, but the owner of the fetched association was not present in the select list"
+			 *  
+			 *  Ocorre por causa da execução da query count, onde a mesma não está inclusa no fetch para dados dp Pedido.
+			 *  
+			 *  "select count(pedido0_.id) as col_0_0_ from pedido pedido0_ where pedido0_.restaurante_id=1 and pedido0_.data_criacao>=?"
+			 */
+			if (Pedido.class.equals(query.getResultType())) {
+				// Para evitar N+1
+				root.fetch("restaurante").fetch("cozinha");
+				root.fetch("cliente");
+			}
 			
 			var predicates = new ArrayList<Predicate>();
 			
