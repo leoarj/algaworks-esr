8adf7f0f0bc42fa06ec4dd8c7de5d600af38472e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-25 22:51:32 -0400

    feat: Add local storage service implementation

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +35,7 @@ public class RestauranteProdutoFotoController {
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
+			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 		
 		MultipartFile arquivo = fotoProdutoInput.getArquivo();
@@ -53,7 +55,7 @@ public class RestauranteProdutoFotoController {
 //				.nomeArquivo(arquivo.getOriginalFilename())
 //			.build();
 		
-		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto);
+		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto, arquivo.getInputStream());
 		
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
 	}
