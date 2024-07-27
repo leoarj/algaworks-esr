80a1dd92c8b62b5b94702090416d97b4527ad266
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-26 20:46:42 -0400

    refactor: apply changes to FileContentType constraint

--- algafood-api/src/main/java/com/algaworks/algafood/core/validation/FileContentType.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/validation/FileContentType.java
@@ -17,13 +17,16 @@ import javax.validation.Payload;
 /**
  * O elemento anotado deve respeitar os formatos de arquivo informados.<b></b>
  * @implNote Chave <b>FileContentType.message</b> para personalizar mensagem de validação no arquivo de mensagens.
+ * @implNote Expressão <b>${allowedValues}</b> para interpolar as extensões de arquivos permitidos na mensagem de validação.
  * @implSpec Mais detalhes na implementação {@link FileContentTypeValidator}
  */
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
 @Constraint(validatedBy = { FileContentTypeValidator.class })
 public @interface FileContentType {
-
+	
+	public static final String ALLOWED_CONTENT_TYPES_EXPRESSION_VARIABLE = "allowedValues";
+	
 	String message() default "{FileContentType.message}";
 
 	Class<?>[] groups() default { };
--- algafood-api/src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
@@ -11,33 +11,27 @@ import org.springframework.web.multipart.MultipartFile;
 /**
  * Implementação para validar elemento anotado com {@link FileContentType}
  * @implNote Utiliza customização do contexto de validação para personalizar mensagens.
- * @implNote Utilize a expressão <b>${allowedMediaTypes}</b> para interpolar as extensões de arquivos permitidos na mensagem de validação.
  * @see {@linkplain https://docs.jboss.org/hibernate/validator/6.2/reference/en-US/html_single/#_custom_contexts}
  */
 public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
 
-	private static final Class<HibernateConstraintValidatorContext>
-		CONSTRAINT_VALIDATOR_CONTEXT_PROVIDER_CLASS = HibernateConstraintValidatorContext.class;
-	
-	private static final String ALLOWED_MEDIA_TYPES_EXPRESSION_VARIABLE_NAME = "allowedMediaTypes";
-	
-	private Set<String> allowedMediaTypes;
+	private Set<String> allowedContentTypes;
 	
 	@Override
 	public void initialize(FileContentType constraintAnnotation) {
-		allowedMediaTypes = Set.of(constraintAnnotation.allowed());
+		allowedContentTypes = Set.of(constraintAnnotation.allowed());
 	}
 	
 	@Override
 	public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
 		// Retorna instância do provedor de contexto de validação para acesso a APIs específicas (Hibernate Validator)
 		context
-			.unwrap(CONSTRAINT_VALIDATOR_CONTEXT_PROVIDER_CLASS)
-			.addExpressionVariable(ALLOWED_MEDIA_TYPES_EXPRESSION_VARIABLE_NAME, allowedMediaTypes.toString()); //${}
+			.unwrap(HibernateConstraintValidatorContext.class)
+			.addExpressionVariable(FileContentType.ALLOWED_CONTENT_TYPES_EXPRESSION_VARIABLE, allowedContentTypes.toString()); //${}
 			// ou
 			//.addMessageParameter(ALLOWED_MEDIA_TYPES_EXPRESSION_VARIABLE_NAME, allowedMediaTypes.toString()); //{}
 		
-		return value == null || allowedMediaTypes.contains(value.getContentType());
+		return value == null || allowedContentTypes.contains(value.getContentType());
 	}
 
 }
