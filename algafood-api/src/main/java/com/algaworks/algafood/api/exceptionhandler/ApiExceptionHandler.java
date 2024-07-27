aae82a0282d74881783b0ba5e32eeef6c23202f1
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-27 14:30:33 -0400

    feat: Add handleHttpMediaTypeNotAcceptable handler

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -44,6 +45,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	 */
 	@Autowired
 	private MessageSource messageSource;
+	
+	/**
+	 * Para tratar corretamente:
+	 * "org.springframework.web.HttpMediaTypeNotAcceptableException: Could not find acceptable representation"
+	 */
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(status).headers(headers).build();
+	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
