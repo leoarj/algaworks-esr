d677e203c856392a662e56b56001db396543d2c6
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-07-28 18:20:53 -0400

    feat: Add email sending to orders confirmation

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.service.EnvioEmailService.Mensagem;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +13,22 @@ import lombok.RequiredArgsConstructor;
 public class FluxoPedidoService {
 
 	private final EmissaoPedidoService emissaoPedidoService;
+	
+	private final EnvioEmailService envioEmailService;
 
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+		
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("O pedido de código <strong>"
+						+ pedido.getCodigo() + "</strong> foi confirmado!")
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+		
+		envioEmailService.enviar(mensagem);
 	}
 
 	@Transactional
