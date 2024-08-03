060afcc71b5dab9bd444b52abf8c98ee10459d47
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-03 15:53:26 -0400

    feat(JS client): Add test call to list endpoint Formas-Pagamento

--- algafood-api-simple-js-client/client.js
+++ algafood-api-simple-js-client/client.js
@@ -1,23 +1,29 @@
-function consultaRestaurantes() {
-	$.ajax({
-		url: "http://api.algafood.local:8080/restaurantes",
-		type: "get",
-		
-		success: function(response) {
-			$("#conteudo").text(JSON.stringify(response));
-		}
-	});
+function consultar() {
+  $.ajax({
+	url: "http://api.algafood.local:8080/formas-pagamento",
+	type: "get",
+	
+	success: function(response) {
+		preencherTabela(response);
+	}
+  });
 }
 
-function fecharRestaurante() {
-	$.ajax({
-		url: "http://api.algafood.local:8080/restaurantes/1/fechamento",
-		type: "put",
-		
-		success: function(response) {
-			alert("Restaurante foi fechado!");
-		}
-	});
+
+function preencherTabela(formasPagamento) {
+  $("#tabela tbody tr").remove();
+
+  $.each(formasPagamento, function(i, formaPagamento) {
+    var linha = $("<tr>");
+
+    linha.append(
+      $("<td>").text(formaPagamento.id),
+      $("<td>").text(formaPagamento.descricao)
+    );
+
+    linha.appendTo("#tabela");
+  });
 }
 
-$("#botao").click(consultaRestaurantes);
+
+$("#btn-consultar").click(consultar);
