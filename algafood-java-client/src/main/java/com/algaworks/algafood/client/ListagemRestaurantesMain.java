967889374c309caccf017eda69e6ce31f9798a58
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-03 18:47:23 -0400

    feat(Java client): Add RestClientResponseException handling

--- algafood-java-client/src/main/java/com/algaworks/algafood/client/ListagemRestaurantesMain.java
+++ algafood-java-client/src/main/java/com/algaworks/algafood/client/ListagemRestaurantesMain.java
@@ -2,17 +2,27 @@ package com.algaworks.algafood.client;
 
 import org.springframework.web.client.RestTemplate;
 
+import com.algaworks.algafood.client.api.ClientApiException;
 import com.algaworks.algafood.client.api.RestauranteClient;
 
 public class ListagemRestaurantesMain {
 
 	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplate();
-		
-		RestauranteClient restauranteClient = new RestauranteClient(
-				restTemplate, "http://api.algafood.local:8080");
-		
-		restauranteClient.listar().stream()
-			.forEach(System.out::println);
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			
+			RestauranteClient restauranteClient = new RestauranteClient(
+					restTemplate, "http://api.algafood.local:8080");
+			
+			restauranteClient.listar().stream()
+				.forEach(System.out::println);
+		} catch (ClientApiException e) {
+			if (e.getProblem() != null) {
+				System.out.println(e.getProblem().getUserMessage());
+			} else {
+				System.out.println("Erro desconhecido");
+				e.printStackTrace();
+			}
+		}
 	}
 }
