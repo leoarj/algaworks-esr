479d9f209682919b91187d052f63091fa9e1dee3
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-10 14:15:46 -0400

    feat: Add ApiInfo in SpringFoxConfig

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.algaworks.algafood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,7 +24,20 @@ public class SpringFoxConfig {
 				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 		        .paths(PathSelectors.any())
 //		          .paths(PathSelectors.ant("/restaurantes/*"))
-					.build();
+					.build()
+				.apiInfo(apiInfo());
+	}
+	
+	/**
+	 * Adiciona informações da API (substituirá informações na UI da documentação)
+	 */
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("AlgaFood API")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
+				.build();
 	}
 	
 }
