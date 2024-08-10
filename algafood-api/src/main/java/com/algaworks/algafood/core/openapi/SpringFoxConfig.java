fe6bbcfdad5c0b6a232de7feca0e3ebc35af8053
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-10 17:37:06 -0400

    feat: Add ProblemDetails to Swagger documentation

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -9,6 +9,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -17,6 +21,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -30,6 +35,8 @@ public class SpringFoxConfig {
 
 	@Bean
 	public Docket apiDocket() {
+		var typeResolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.OAS_30)
 				.select()
 //					.apis(RequestHandlerSelectors.any()) // qualquer controllador
@@ -42,8 +49,14 @@ public class SpringFoxConfig {
 				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
 		        .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
 		        .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+		        .additionalModels(typeResolver.resolve(Problem.class)) // registra classe de ProblemDetail para a documentação
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades")); // para personalizar as tags referente a recursos, na UI da documentação
+	}
+	
+	@Bean
+	public JacksonModuleRegistrar springFoxJacksonConfig() {
+		return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
 	}
 	
 	/**
