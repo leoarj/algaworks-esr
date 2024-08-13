d9cc104432b950611cb68dee0fc07788cbee0bde
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-12 21:18:29 -0400

    feat: Add OpenApiDoc to UsuarioGrupoController

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.GrupoModelAssembler;
 import com.algaworks.algafood.api.model.GrupoModel;
+import com.algaworks.algafood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.service.CadastroUsuarioService;
 
@@ -20,8 +22,9 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = "/usuarios/{usuarioId}/grupos",
+	produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 	
 	private final CadastroUsuarioService cadastroUsuarioService;
 
@@ -36,13 +39,13 @@ public class UsuarioGrupoController {
 	
 	@PutMapping("/{grupoId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void associarGrupo(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+	public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
 		cadastroUsuarioService.associarGrupo(usuarioId, grupoId);
 	}
 	
 	@DeleteMapping("/{grupoId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void desassociarGrupo(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+	public void desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
 		cadastroUsuarioService.desassociarGrupo(usuarioId, grupoId);
 	}
 }
