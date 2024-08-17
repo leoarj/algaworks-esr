a5fe59ef0a7aa3ef16edeff7fb53bf261123f4cb
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-17 16:30:48 -0400

    feat: Add HyperMedia to Cozinhas paged resource

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/CozinhaModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/CozinhaModelAssembler.java
@@ -1,27 +1,40 @@
 package com.algaworks.algafood.api.assembler;
 
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.controller.CozinhaController;
 import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.domain.model.Cozinha;
 
 @Component
-public class CozinhaModelAssembler {
+public class CozinhaModelAssembler extends RepresentationModelAssemblerSupport<Cozinha, CozinhaModel> {
 
 	@Autowired
 	private ModelMapper modelMapper;
+
+	public CozinhaModelAssembler() {
+		super(CozinhaController.class, CozinhaModel.class);
+	}
 	
+	@Override
 	public CozinhaModel toModel(Cozinha cozinha) {
-		return modelMapper.map(cozinha, CozinhaModel.class);
+		CozinhaModel cozinhaModel = createModelWithId(cozinha.getId(), cozinha);
+		
+		modelMapper.map(cozinha, cozinhaModel);
+		
+		cozinhaModel.add(WebMvcLinkBuilder.linkTo(CozinhaController.class)
+				.withRel("cozinhas"));
+		
+		return cozinhaModel;
 	}
 	
-	public List<CozinhaModel> toCollectionModel(List<Cozinha> cozinhas) {
-		return cozinhas.stream()
-				.map(this::toModel)
-				.toList();
-	}
+//	public List<CozinhaModel> toCollectionModel(List<Cozinha> cozinhas) {
+//		return cozinhas.stream()
+//				.map(this::toModel)
+//				.toList();
+//	}
 }
