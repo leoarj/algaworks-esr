241a4690f46b0fa9d14366b7b5ba6bc6b0c44cdf
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-17 21:46:02 -0400

    feat: Add class to build HyperMedia links

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -2,15 +2,11 @@ package com.algaworks.algafood.api.assembler;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.TemplateVariable;
-import org.springframework.hateoas.TemplateVariable.VariableType;
-import org.springframework.hateoas.TemplateVariables;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.controller.CidadeController;
 import com.algaworks.algafood.api.controller.FormaPagamentoController;
 import com.algaworks.algafood.api.controller.PedidoController;
@@ -25,6 +21,9 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 
 	@Autowired
 	private ModelMapper modelMapper;
+	
+	@Autowired
+	private AlgaLinks algaLinks;
 
 	public PedidoModelAssembler() {
 		super(PedidoController.class, PedidoModel.class);
@@ -36,21 +35,7 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 		
 		modelMapper.map(pedido, pedidoModel);
 		
-		TemplateVariables pageVariables = new TemplateVariables(
-				new TemplateVariable("page", VariableType.REQUEST_PARAM),
-				new TemplateVariable("size", VariableType.REQUEST_PARAM),
-				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
-		
-		TemplateVariables filterVariables = new TemplateVariables(
-				new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
-				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
-		
-		String pedidosUrl = WebMvcLinkBuilder.linkTo(PedidoController.class).toUri().toString();
-		
-		pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl,
-				pageVariables.concat(filterVariables)), "pedidos"));
+		pedidoModel.add(algaLinks.linkToPedidos());
 		
 //		pedidoModel.add(WebMvcLinkBuilder.linkTo(PedidoController.class)
 //				.withRel("pedidos"));
