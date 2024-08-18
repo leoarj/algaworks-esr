81fe10eb8fa7ea3efdf5364ef5008777d00a979e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-17 21:03:28 -0400

    refactor: Add template variables to HyperMedia
    Add template variables to HyperMedia links to pedidos collection
    resource.

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -2,6 +2,11 @@ package com.algaworks.algafood.api.assembler;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariable.VariableType;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
@@ -31,8 +36,17 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 		
 		modelMapper.map(pedido, pedidoModel);
 		
-		pedidoModel.add(WebMvcLinkBuilder.linkTo(PedidoController.class)
-				.withRel("pedidos"));
+		TemplateVariables pageVariables = new TemplateVariables(
+				new TemplateVariable("page", VariableType.REQUEST_PARAM),
+				new TemplateVariable("size", VariableType.REQUEST_PARAM),
+				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+		
+		String pedidosUrl = WebMvcLinkBuilder.linkTo(PedidoController.class).toUri().toString();
+		
+		pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl, pageVariables), "pedidos"));
+		
+//		pedidoModel.add(WebMvcLinkBuilder.linkTo(PedidoController.class)
+//				.withRel("pedidos"));
 		
 		pedidoModel.getRestaurante().add(WebMvcLinkBuilder.linkTo(
 				WebMvcLinkBuilder.methodOn(RestauranteController.class)
