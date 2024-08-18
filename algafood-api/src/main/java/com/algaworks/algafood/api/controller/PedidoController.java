5fdd64355a95ad28d12fb3206a880e7d6d61dad7
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-17 20:03:15 -0400

    fix: Fix HyperMedia in Pedidos resource when using sorting

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -26,6 +26,7 @@ import com.algaworks.algafood.api.model.PedidoModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.model.input.PedidoInput;
 import com.algaworks.algafood.api.openapi.controller.PedidoControllerOpenApi;
+import com.algaworks.algafood.core.data.PageWrapper;
 import com.algaworks.algafood.core.data.PageableTranslator;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
@@ -57,9 +58,13 @@ public class PedidoController implements PedidoControllerOpenApi {
 	@GetMapping
 	public PagedModel<PedidoResumoModel> pesquisar(PedidoFilter filtro,
 			@PageableDefault(size = 10) Pageable pageable) {
-		pageable = traduzirPageable(pageable);
+		Pageable pageableTraduzido = traduzirPageable(pageable);
 		
-		Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro), pageable);
+		Page<Pedido> pedidosPage = pedidoRepository.findAll(
+				PedidoSpecs.usandoFiltro(filtro), pageableTraduzido);
+		
+		// Repassa pageable original para construção dos links de HyperMedia
+		pedidosPage = new PageWrapper<>(pedidosPage, pageable);
 				
 		PagedModel<PedidoResumoModel> pedidosResumoPagedModel = pagedResourcesAssembler
 				.toModel(pedidosPage, pedidoResumoModelAssembler);
