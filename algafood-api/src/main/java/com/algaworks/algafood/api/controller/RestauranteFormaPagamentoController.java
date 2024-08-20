e407c841861f427a47ec71a2f8fe3cdd4d9b0985
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-19 21:23:19 -0400

    fix: Fix link relation to sub-resource of payment methods
    Fix HyperMedia link relation to sub-resource of payment methods of
    restaurant.

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
 import com.algaworks.algafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
@@ -28,11 +29,16 @@ public class RestauranteFormaPagamentoController implements RestauranteFormaPaga
 	
 	private final FormaPagamentoModelAssembler formaPagamentoModelAssembler;
 	
+	private final AlgaLinks algaLinks;
+	
 	@GetMapping
 	public CollectionModel<FormaPagamentoModel> listar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 		
-		return formaPagamentoModelAssembler.toCollectionModel(restaurante.getFormasPagamento());
+		return formaPagamentoModelAssembler.toCollectionModel(
+				restaurante.getFormasPagamento())
+				.removeLinks()
+				.add(algaLinks.linkToRestauranteFormasPagamento(restauranteId, "formas-pagamento"));
 	}
 	
 	@DeleteMapping("/{formaPagamentoId}")
