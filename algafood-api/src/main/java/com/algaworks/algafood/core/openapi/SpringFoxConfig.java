beb1b1bc8e84ea9fecacd638fdf295dcb7908e5b
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-24 18:19:24 -0400

    feat: Add LinksModelOpenApi substitute

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -16,6 +16,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Links;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,6 +27,7 @@ import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.openapi.model.CozinhasModelOpenApi;
+import com.algaworks.algafood.api.openapi.model.LinksModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PageableModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PedidosResumoModelOpenApi;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
@@ -85,6 +88,7 @@ public class SpringFoxConfig {
 		        		URL.class, URI.class, URLStreamHandler.class, Resource.class,
 		        		File.class, InputStream.class)
 		        .directModelSubstitute(Pageable.class, PageableModelOpenApi.class) // substituição para documentação de Pageable
+		        .directModelSubstitute(Links.class, LinksModelOpenApi.class)
 		        .alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(Page.class, CozinhaModel.class),
 						CozinhasModelOpenApi.class))
