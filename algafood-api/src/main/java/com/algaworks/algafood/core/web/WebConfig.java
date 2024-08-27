fcce68b937786dec3ffe273b7682017c381bcc8d
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-26 20:46:59 -0400

    refactor: Remove config for media type API version

--- algafood-api/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -5,7 +5,6 @@ import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,14 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
 			.allowedMethods("*"); // habilita para todos os verbos HTTP
 //			.allowedOrigins("*") // habilita para qualquer origem (padrão)
 //			.maxAge(30); // define o tempo de vida (em segundos) do cache preflight para os clients
-	}
-	
-	/**
-	 * Para definir a versão padrão caso não seja especificadoo Accept na requisição.
-	 */
-	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATION_JSON);
 	}
 	
 	/**
