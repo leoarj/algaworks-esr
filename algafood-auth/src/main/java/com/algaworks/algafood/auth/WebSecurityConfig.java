6cd92aa90d61509a476314712a9f163de196108a
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-31 18:22:48 -0400

    feat: Add AuthorizationManager bean

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,5 +38,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	/**
+	 * Para repassar o bean referente ao Authentication Manager (Necessário no fluxo "password credentials").
+	 */
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 }
