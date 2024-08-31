6729129a5a34662b6671232d2b5850f40b120441
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-31 14:45:04 -0400

    feat: Add inMemory authentication example

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Componente para configurar a segurança na API<br>
@@ -15,6 +19,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication() // configurando autenticação em memória (testes)
+			.withUser("leandro")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMNIN")
+			.and()
+			.withUser("joao")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN");
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -32,5 +48,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				
 			.and()
 				.csrf().disable(); // desabilitar o Cross-site Request Forgery (evitar transporte de sessão)
+	}
+	
+	/**
+	 * Para gerar o bean referente a codificação da senha.
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
