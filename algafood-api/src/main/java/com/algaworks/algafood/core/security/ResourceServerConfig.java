995b51575716342462fedc49f78798dde5d0a85a
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-08-31 20:26:54 -0400

    refactor: Add ResourceServer to api

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -17,44 +17,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * */
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication() // configurando autenticação em memória (testes)
-			.withUser("leandro")
-				.password(passwordEncoder().encode("123"))
-				.roles("ADMNIN")
-			.and()
-			.withUser("joao")
-				.password(passwordEncoder().encode("123"))
-				.roles("ADMIN");
-	}
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.httpBasic()
-			
-			.and()
-			
 			.authorizeRequests()
-				.antMatchers("/v1/cozinhas/**").permitAll() // torna recurso de cozinhas público
 				.anyRequest().authenticated() // restringe qualquer outro endpoint a ter somente acesso com autenticação
 				
 			.and()
-			.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // para não guardar estado de sessão
-				
-			.and()
-				.csrf().disable(); // desabilitar o Cross-site Request Forgery (evitar transporte de sessão)
-	}
-	
-	/**
-	 * Para gerar o bean referente a codificação da senha.
-	 */
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+			.oauth2ResourceServer().opaqueToken();
 	}
 }
