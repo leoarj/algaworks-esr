c23af1495695b03e9e2cf1d9cd12591d8fb3bc9e
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-09-05 22:21:00 -0400

    raw: Update users password to Bcrypt encrypted in DB

--- algafood-api/src/main/resources/db/testdata/afterMigrate.sql
+++ algafood-api/src/main/resources/db/testdata/afterMigrate.sql
@@ -78,13 +78,13 @@ insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Esp
 insert into grupo (nome) values ('Gerente'), ('Vendedor'), ('Secretária'), ('Cadastrador');
 
 insert into usuario (id, nome, email, senha, data_cadastro) values
-(1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp),
-(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp),
-(3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp),
-(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp),
-(5, 'Manoel Lima', 'manoel.loja@gmail.com', '123', utc_timestamp),
-(6, 'Débora Mendonça', 'develop.test.leandro+debora@gmail.com', '123', utc_timestamp),
-(7, 'Carlos Lima', 'develop.test.leandro+carlos@gmail.com', '123', utc_timestamp);
+(1, 'João da Silva', 'joao.ger@algafood.com', '$2a$12$RjhIbd6kRGQ2UgykhrIJl.Cva2/yc7ESkZT2jEYtDCtaU83SoykMC', utc_timestamp),
+(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '$2a$12$RjhIbd6kRGQ2UgykhrIJl.Cva2/yc7ESkZT2jEYtDCtaU83SoykMC', utc_timestamp),
+(3, 'José Souza', 'jose.aux@algafood.com', '$2a$12$RjhIbd6kRGQ2UgykhrIJl.Cva2/yc7ESkZT2jEYtDCtaU83SoykMC', utc_timestamp),
+(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '$2a$12$RjhIbd6kRGQ2UgykhrIJl.Cva2/yc7ESkZT2jEYtDCtaU83SoykMC', utc_timestamp),
+(5, 'Manoel Lima', 'manoel.loja@gmail.com', '$2a$12$RjhIbd6kRGQ2UgykhrIJl.Cva2/yc7ESkZT2jEYtDCtaU83SoykMC', utc_timestamp),
+(6, 'Débora Mendonça', 'develop.test.leandro+debora@gmail.com', '$2a$12$RjhIbd6kRGQ2UgykhrIJl.Cva2/yc7ESkZT2jEYtDCtaU83SoykMC', utc_timestamp),
+(7, 'Carlos Lima', 'develop.test.leandro+carlos@gmail.com', '$2a$12$RjhIbd6kRGQ2UgykhrIJl.Cva2/yc7ESkZT2jEYtDCtaU83SoykMC', utc_timestamp);
 
 INSERT INTO grupo_permissao (grupo_id, permissao_id) VALUES (1, 1), (1, 2), (2, 1), (2, 2), (3, 1);
 
