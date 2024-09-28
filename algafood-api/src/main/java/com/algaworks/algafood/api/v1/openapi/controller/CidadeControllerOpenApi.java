3abcdf7f1c46af518c062c5791093dcb07d07e10
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-09-28 14:05:08 -0400

    refactor: Add problem model representation for error status

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -17,11 +17,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 /**
  * Interface para desacoplar o controlador das anotações referentes a documentação do OpenAPI.
  */
-//@Api(tags = "Cidades") // associa o controlador como um recurso da tag especificada no configuration
 
 // Referencia o esquema de segurança definido na configuração do bean do Spring Doc.
 @SecurityRequirement(name = "security_auth")
-@Tag(name = "Cidades")
+@Tag(name = "Cidades") // associa o controlador como um recurso da tag especificada no configuration
 public interface CidadeControllerOpenApi {
 
 	@Operation(summary = "Lista as cidades")
@@ -31,47 +30,43 @@ public interface CidadeControllerOpenApi {
 			responses = {
 					@ApiResponse(responseCode = "200"),
 					@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
-					content = @Content(schema = @Schema(ref = "Problema")))
+						content = @Content(schema = @Schema(ref = "Problema"))),
+					@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+						content = @Content(schema = @Schema(ref = "Problema")))
 					
 			})
-//	@ApiResponses({
-//		@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
-//				content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
-//		@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
-//			content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
-//	})
 	CidadeModel buscar(
 			@Parameter(description = "ID de uma cidade", example = "1", required = true)
 			Long cidadeId);
 	
 	@Operation(summary = "Cadastra uma cidade",
 			description = "Cadastro de uma cidade, necessita de um estado e um nome válido")
-//	@ApiResponses({
-//		@ApiResponse(responseCode = "201", description = "Cidade cadastrada"),
-//	})
 	CidadeModel adicionar(
 			@RequestBody(description = "Representação de uma nova cidade", required = true)
 			CidadeInput cidadeInput);
 	
-	@Operation(summary = "Atualiza uma cidade por ID")
-//	@ApiResponses({
-//		@ApiResponse(responseCode = "200", description = "Cidade atualizada"),
-//		@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
-//			content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
-//	})
+	@Operation(summary = "Atualiza uma cidade por ID",
+			responses = {
+					@ApiResponse(responseCode = "200"),
+					@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
+							content = @Content(schema = @Schema(ref = "Problema"))),
+					@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+					content = @Content(schema = @Schema(ref = "Problema")))
+			})
 	CidadeModel atualizar(
 			@Parameter(description = "ID de uma cidade", example = "1", required = true)
 			Long cidadeId,
-			
 			@RequestBody(description = "Representação de uma cidade com os novos dados", required = true)
 			CidadeInput cidadeInput);
 	
-	@Operation(summary = "Exclui uma cidade por ID")
-//	@ApiResponses({
-//		@ApiResponse(responseCode = "204", description = "Cidade excluída"),
-//		@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
-//			content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
-//	})
+	@Operation(summary = "Exclui uma cidade por ID",
+			responses = {
+					@ApiResponse(responseCode = "204"),
+					@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
+							content = @Content(schema = @Schema(ref = "Problema"))),
+					@ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+					content = @Content(schema = @Schema(ref = "Problema")))
+			})
 	void remover(
 			@Parameter(description = "ID de uma cidade", example = "1", required = true)
 			Long cidadeId);
--- algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.core.springdoc;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +10,7 @@ import org.springdoc.core.customizers.OpenApiCustomiser;
 //import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 
@@ -22,6 +25,8 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
@@ -43,6 +48,11 @@ import io.swagger.v3.oas.models.tags.Tag;
 				})))
 public class SpringDocConfig {
 
+	private static final String BAD_REQUEST_RESPONSE = "BadRequestResponse";
+    private static final String NOT_FOUND_RESPONSE = "NotFoundResponse";
+    private static final String NOT_ACCEPTABLE_RESPONSE = "NotAcceptableResponse";
+    private static final String INTERNAL_SERVER_ERROR_RESPONSE = "InternalServerErrorResponse";
+	
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
@@ -59,9 +69,10 @@ public class SpringDocConfig {
 								.url("https://algaworks.com")
 						).tags(Arrays.asList(
 								new Tag().name("Cidades").description("Gerencia as cidades")
-						)).components(new Components().schemas(
-								gerarSchemas()
-						));
+						)).components(new Components()
+								.schemas(gerarSchemas())
+								.responses(gerarResponses())
+						);
 	}
 	
 	/**
@@ -75,30 +86,22 @@ public class SpringDocConfig {
 					.forEach(pathItem -> pathItem.readOperationsMap().forEach((httpMethod, operation) -> {
 						ApiResponses responses = operation.getResponses();
 
-						responses.addApiResponse("500", new ApiResponse().description("Erro interno no servidor"));
+						responses.addApiResponse("500", new ApiResponse().$ref(INTERNAL_SERVER_ERROR_RESPONSE));
 						
 						switch (httpMethod) {
 						case GET:
-							responses.addApiResponse("404", new ApiResponse().description("Recurso não encontrado"));
-							responses.addApiResponse("406", new ApiResponse().description(
-									"Recurso não possui representação que poderia ser aceita pelo consumidor"));
+							responses.addApiResponse("406", new ApiResponse().$ref(NOT_ACCEPTABLE_RESPONSE));
 							break;
 						case POST:
-							responses.addApiResponse("400", new ApiResponse().description("Requisição inválida"));
-
+							responses.addApiResponse("400", new ApiResponse().$ref(BAD_REQUEST_RESPONSE));
 							break;
-
 						case PUT:
-							responses.addApiResponse("404", new ApiResponse().description("Recurso não encontrado"));
-							responses.addApiResponse("400", new ApiResponse().description("Requisição inválida"));
-
+							responses.addApiResponse("400", new ApiResponse().$ref(BAD_REQUEST_RESPONSE));
 							break;
-
-						case DELETE:
-							responses.addApiResponse("404", new ApiResponse().description("Recurso não encontrado"));
-							break;
+//						case DELETE:
+//							break;
 						default:
-							responses.addApiResponse("500", new ApiResponse().description("Erro interno no servidor"));
+							responses.addApiResponse("500", new ApiResponse().$ref(INTERNAL_SERVER_ERROR_RESPONSE));
 							break;
 						}
 
@@ -176,7 +179,7 @@ public class SpringDocConfig {
 	
 	/**
 	 * Gera a associação de referências (nome do schema) e a class correspondente, expondo a estrutura para o OpenApi.
-	 * Utilizado como compomente na configuração do OpenApi, para saber como será representado o modelo na documentação.
+	 * Utilizado como compomente na configuração do OpenApi, para saber como será representado o modelo de {@link Problem} na documentação.
 	 */
 	private Map<String, Schema> gerarSchemas() {
 		final Map<String, Schema> schemaMap = new HashMap<>();
@@ -188,6 +191,38 @@ public class SpringDocConfig {
 		schemaMap.putAll(problemObjectSchema);
 		
 		return schemaMap;
+	}
+	
+	/**
+	 * Gera associação do modelo de representação do objeto de problema,
+	 * com referências que serão apontadas em ApiResponse dos verbos HTTP,
+	 * na customização da configuração do OpenApi (OpenApiCustomizer). 
+	 */
+	private Map<String , ApiResponse> gerarResponses() {
+		final Map<String , ApiResponse> apiResponseMap = new HashMap<>();
+		
+		// Realiza associação da estrutura do objeto problema com Media Type application/json
+		Content content = new Content()
+				.addMediaType(APPLICATION_JSON_VALUE,
+						new MediaType().schema(new Schema<Problem>().$ref("Problema")));
+		
+		apiResponseMap.put(BAD_REQUEST_RESPONSE, new ApiResponse()
+				.description("Requisição inválida")
+				.content(content));
+		
+		apiResponseMap.put(NOT_FOUND_RESPONSE, new ApiResponse()
+				.description("Recurso não encontrado")
+				.content(content));
+		
+		apiResponseMap.put(NOT_ACCEPTABLE_RESPONSE, new ApiResponse()
+				.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+				.content(content));
+		
+		apiResponseMap.put(INTERNAL_SERVER_ERROR_RESPONSE, new ApiResponse()
+				.description("Erro interno no servidor")
+				.content(content));
+		
+		return apiResponseMap;
 	}
 	
 }
