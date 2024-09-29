48e7722fb95cbea91b579e50f73304def94221d8
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-09-29 10:16:00 -0400

    refactor: Change EstadoController to SpringDoc

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstadoControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstadoControllerOpenApi.java
@@ -5,54 +5,61 @@ import org.springframework.hateoas.CollectionModel;
 import com.algaworks.algafood.api.v1.model.EstadoModel;
 import com.algaworks.algafood.api.v1.model.input.EstadoInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-//@Api(tags = "Estados")
+@Tag(name = "Estados")
 @SecurityRequirement(name = "security_auth")
 public interface EstadoControllerOpenApi {
 
-//    @ApiOperation("Lista os estados")
+    @Operation(summary = "Lista os estados")
     CollectionModel<EstadoModel> listar();
 
-//    @ApiOperation("Busca um estado por ID")
-//    @ApiResponses({
-//		@ApiResponse(responseCode = "400", description = "ID do estado inválido",
-//				content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
-//		@ApiResponse(responseCode = "404", description = "Estado não encontrado",
-//			content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
-//	})
+    @Operation(summary = "Busca um estado por ID",
+    		responses = {
+    				@ApiResponse(responseCode = "200"),
+    				@ApiResponse(responseCode = "400", description = "ID do estado inválido",
+    						content = @Content(schema = @Schema(ref = "Problema"))),
+    				@ApiResponse(responseCode = "404", description = "Estado não encontrado",
+					content = @Content(schema = @Schema(ref = "Problema")))
+    		})
     EstadoModel buscar(
-//            @ApiParam(value = "ID de um estado", example = "1", required = true)
+    		@Parameter(description = "ID de um estado", example = "1", required = true)
             Long estadoId);
 
-//    @ApiOperation("Cadastra um estado")
-//    @ApiResponses({
-//		@ApiResponse(responseCode = "201", description = "Estado cadastrado"),
-//	})
+    @Operation(summary = "Cadastra um estado",
+    		responses = {
+    				@ApiResponse(responseCode = "201", description = "Estado cadastrado")
+    		})
     EstadoModel adicionar(
-//            @ApiParam(name = "corpo", value = "Representação de um novo estado", required = true)
+            @RequestBody(description = "Representação de um novo estado", required = true)
             EstadoInput estadoInput);
 
-//    @ApiOperation("Atualiza um estado por ID")
-//    @ApiResponses({
-//		@ApiResponse(responseCode = "200", description = "Estado atualizado"),
-//		@ApiResponse(responseCode = "404", description = "Estado não encontrado",
-//			content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
-//	})
+    @Operation(summary = "Atualiza um estado por ID",
+    		responses = {
+    				@ApiResponse(responseCode = "200", description = "Estado atualizado"),
+    				@ApiResponse(responseCode = "404", description = "Estado não encontrado",
+    						content = @Content(schema = @Schema(ref = "Problema")))
+    		})
     EstadoModel atualizar(
-//            @ApiParam(value = "ID de um estado", example = "1", required = true)
+    		@Parameter(description = "ID de um estado", example = "1", required = true)
             Long estadoId,
-            
-//            @ApiParam(name = "corpo", value = "Representação de um estado com os novos dados", required = true)
+            @RequestBody(description = "Representação de um estado com os novos dados", required = true)
             EstadoInput estadoInput);
 
-//    @ApiOperation("Exclui um estado por ID")
-//    @ApiResponses({
-//		@ApiResponse(responseCode = "204", description = "Estado excluído"),
-//		@ApiResponse(responseCode = "404", description = "Estado não encontrado",
-//			content = @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
-//	})
+    @Operation(summary = "Exclui um estado por ID",
+    		responses = {
+    				@ApiResponse(responseCode = "204", description = "Estado excluído"),
+    				@ApiResponse(responseCode = "404", description = "Estado não encontrado",
+    						content = @Content(schema = @Schema(ref = "Problema")))
+    		})
     void remover(
-//            @ApiParam(value = "ID de um estado", example = "1", required = true)
+            @Parameter(description = "ID de um estado", example = "1", required = true)
             Long estadoId);
 }        
