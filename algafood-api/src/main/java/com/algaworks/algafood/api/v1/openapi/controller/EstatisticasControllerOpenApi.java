32f7a7f0d4bd92296f9d60d76048e9f40c294e67
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-09-29 15:21:27 -0400

    refactor: Change EstatisticasController to SpringDoc

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
@@ -2,37 +2,72 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.v1.model.rootentrypoint.EstatisticasModel;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-//@Api(tags = "Estatísticas")
+@Tag(name = "Estatísticas")
 @SecurityRequirement(name = "security_auth")
 public interface EstatisticasControllerOpenApi {
 
-//	@ApiOperation(value = "Estatísticas", hidden = true)
+	@Operation(hidden = true)
 	EstatisticasModel estatisticas();
 	
-//    @ApiOperation("Consulta estatísticas de vendas diárias")
-//    @ApiImplicitParams({
-//        @ApiImplicitParam(name = "restauranteId", value = "ID do restaurante", 
-//                example = "1", dataType = "int", dataTypeClass = Integer.class),
-//        @ApiImplicitParam(name = "dataCriacaoInicio", value = "Data/hora inicial da criação do pedido",
-//            example = "2019-12-01T00:00:00Z", dataType = "date-time", dataTypeClass = OffsetDateTime.class),
-//        @ApiImplicitParam(name = "dataCriacaoFim", value = "Data/hora final da criação do pedido",
-//            example = "2019-12-02T23:59:59Z", dataType = "date-time", dataTypeClass = OffsetDateTime.class)
-//    })
+	@Operation(summary = "Consulta estatísticas de vendas diárias",
+			parameters = {
+					@Parameter(
+							in = ParameterIn.QUERY,
+					    		name = "restauranteId",
+					    		description = "ID do restaurante",
+					    		example = "1",
+					    		schema = @Schema(type = "integer"),
+					    		required = false
+							),
+					@Parameter(
+							in = ParameterIn.QUERY,
+					    		name = "dataCriacaoInicio",
+					    		description = "Data/hora inicial da criação do pedido",
+					    		example = "2019-12-01T00:00:00Z",
+					    		schema = @Schema(type = "string", format = "date-time"),
+					    		required = false
+							),
+					@Parameter(
+							in = ParameterIn.QUERY,
+					    		name = "dataCriacaoFim",
+					    		description = "Data/hora final da criação do pedido",
+					    		example = "2019-12-02T23:59:59Z",
+					    		schema = @Schema(type = "string", format = "date-time"),
+					    		required = false
+							)
+			},
+    		responses = {
+				@ApiResponse(responseCode = "200",
+						content = {
+								@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = VendaDiaria.class))),
+								@Content(mediaType = MediaType.APPLICATION_PDF_VALUE, schema = @Schema(type = "string", format = "binary"))
+						})
+    		})
     List<VendaDiaria> consultarVendasDiarias(
+    		@Parameter(hidden = true)
             VendaDiariaFilter filtro,
-            
-//            @ApiParam(value = "Deslocamento de horário a ser considerado na consulta em relação ao UTC",
-//                defaultValue = "+00:00")
+            @Parameter(description = "Deslocamento de horário a ser considerado na consulta em relação ao UTC",
+            		schema = @Schema(type = "string", defaultValue = "+00:00"))
             String timeOffset);
 
+    @Operation(hidden = true)
     ResponseEntity<byte[]> consultarVendasDiariasPdf(
             VendaDiariaFilter filtro,
             String timeOffset);
