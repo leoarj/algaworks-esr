dab3ddf5d5e7089a16329fa5fa3f9763b899f04d
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-09-29 17:05:38 -0400

    refactor: Change CidadeController (v2) to SpringDoc

--- algafood-api/src/main/java/com/algaworks/algafood/api/v2/model/CidadeModelV2.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v2/model/CidadeModelV2.java
@@ -3,19 +3,19 @@ package com.algaworks.algafood.api.v2.model;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-//@ApiModel("CidadeModel")
 @Relation(collectionRelation = "cidades")
 @Setter
 @Getter
 public class CidadeModelV2 extends RepresentationModel<CidadeModelV2> {
 
-//	@ApiModelProperty(example = "1")
+	@Schema(example = "1")
 	private Long idCidade;
 	
-//	@ApiModelProperty(example = "Uberlândia")
+	@Schema(example = "Uberlândia")
 	private String nomeCidade;
 	
 	private Long idEstado;
--- algafood-api/src/main/java/com/algaworks/algafood/api/v2/model/input/CidadeInputV2.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v2/model/input/CidadeInputV2.java
@@ -3,19 +3,19 @@ package com.algaworks.algafood.api.v2.model.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-//@ApiModel("CidadeInput")
 @Setter
 @Getter
 public class CidadeInputV2 {
 
-//	@ApiModelProperty(example = "Uberlândia", required = true)
+	@Schema(example = "Uberlândia")
 	@NotBlank
 	private String nomeCidade;
 	
-//	@ApiModelProperty(example = "1", required = true)
+	@Schema(example = "1")
 	@NotNull
 	private Long idEstado;
 }
