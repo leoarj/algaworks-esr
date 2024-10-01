ab596e95a39d1ae28e8174683415de14e0216d73
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2024-09-30 21:57:05 -0400

    feat: Add resource server config for opaque tokens

---
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Classe de configurações de segurança do Resource Server.
+ */
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class ResourceServerConfig {
+
+	/**
+	 * Configura cadeia de filtros de segurança para o Resource Server.
+	 * Como autenticações de endpoints e tipo/verificação de token.
+	 */
+	@Bean
+	public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/oauth2/**").authenticated()
+			.and()
+			.csrf().disable()
+			.cors().and()
+			.oauth2ResourceServer().opaqueToken();
+		
+		return http.build();
+	}
+	
+}
